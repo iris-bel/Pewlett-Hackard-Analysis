0bf84d9b5b179496c1867cae8413fc202f188dad
iris-bel <belensky.iris@gmail.com>
2022-12-07 17:16:37 -0700

    Adding Queries folder

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,49 @@
+--Create table with titles of soon to retire
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+	LEFT JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE(birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Order by to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE (retirement_titles.to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+-- Find number of employees per title
+SELECT COUNT (ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY ut.count DESC;
+
+-- Find which employees as eligible for mentorship
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+--INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
